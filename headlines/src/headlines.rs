431dc0e52d2dc04adc4bf2bae9e4d8feee317f74
Vivek Yadav <vivek.yadav472@gmail.com>
2022-11-26 01:42:30 +0530

    Header and footer

--- headlines/src/headlines.rs
+++ headlines/src/headlines.rs
@@ -1,6 +1,9 @@
-use eframe::egui;
-use eframe::{egui::{Color32, Align, RichText, Label, Layout, Hyperlink, Separator, FontDefinitions, FontFamily, FontData}};
-use std::{iter::FromIterator};
+use eframe::egui::{self, Button, TopBottomPanel};
+use eframe::egui::{
+    Align, Color32, FontData, FontDefinitions, FontFamily, Hyperlink, Label, Layout, RichText,
+    Separator,
+};
+use std::iter::FromIterator;
 
 pub const PADDING: f32 = 5.0;
 const WHITE: Color32 = Color32::from_rgb(255, 255, 255);
@@ -8,42 +11,50 @@ const CYAN: Color32 = Color32::from_rgb(0, 255, 255);
 
 #[derive(Default)]
 pub struct Headlines {
-    articles: Vec<NewsCardData>
+    articles: Vec<NewsCardData>,
 }
 
 struct NewsCardData {
     title: String,
     desc: String,
-    url: String
+    url: String,
 }
 
 impl Headlines {
     pub fn new() -> Headlines {
         let iter = (0..20).map(|a| NewsCardData {
             title: format!("title{}", a),
-            desc : format!("desc{}", a),
-            url : format!("https://example.com/{}", a)
+            desc: format!("desc{}", a),
+            url: format!("https://example.com/{}", a),
         });
 
         Headlines {
-            articles: Vec::from_iter(iter)
+            articles: Vec::from_iter(iter),
         }
     }
 
-    pub fn configure_fonts(&mut self, ctx : &egui::Context) {
+    pub fn configure_fonts(&mut self, ctx: &egui::Context) {
         let mut font_def = FontDefinitions::default();
-        font_def.font_data.insert("MesloLGS".to_owned(), FontData::from_static(include_bytes!("../../MesloLGS_NF_Regular.ttf")));
-        font_def.families.get_mut(&FontFamily::Proportional).unwrap().insert(0, "MesloLGS".to_string());
+        font_def.font_data.insert(
+            "MesloLGS".to_owned(),
+            FontData::from_static(include_bytes!("../../MesloLGS_NF_Regular.ttf")),
+        );
+        font_def
+            .families
+            .get_mut(&FontFamily::Proportional)
+            .unwrap()
+            .insert(0, "MesloLGS".to_string());
         ctx.set_fonts(font_def);
     }
 
-    pub fn render_news_cards(&self, ui: &mut eframe::egui::Ui){
+    pub fn render_news_cards(&self, ui: &mut eframe::egui::Ui) {
         for a in &self.articles {
             // ui.add_space(PADDING);
             let title = format!("▶ {}", a.title);
             ui.colored_label(WHITE, title);
             ui.add_space(PADDING);
-            let desc = Label::new(RichText::new(&a.desc).text_style(eframe::egui::TextStyle::Button));
+            let desc =
+                Label::new(RichText::new(&a.desc).text_style(eframe::egui::TextStyle::Button));
             ui.add(desc);
 
             ui.style_mut().visuals.hyperlink_color = CYAN;
@@ -54,5 +65,28 @@ impl Headlines {
             ui.add_space(PADDING);
             ui.add(Separator::default());
         }
+    }
+
+    pub fn render_top_panel(&self, ctx: &egui::Context) {
+        TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            ui.add_space(10.);
+            egui::menu::bar(ui, |ui| {
+                ui.with_layout(Layout::left_to_right(Align::Min), |ui| {
+                    ui.add(Label::new(
+                        RichText::new("📓").text_style(eframe::egui::TextStyle::Heading),
+                    ));
+                });
+
+                ui.with_layout(Layout::right_to_left(Align::Min), |ui| {
+                    let close_btn = ui.add(Button::new("❌"));
+                    let refresh_btn = ui.add(Button::new("🔄"));
+                    let theme_btn = ui.add(Button::new("🌙"));
+                });
+
+                ui.add_space(10.);
+            });
+
+            ui.add_space(10.);
+        });
     }
 }
--- headlines/src/main.rs
+++ headlines/src/main.rs
@@ -1,17 +1,57 @@
 mod headlines;
-use crate::headlines::Headlines;
-use eframe::egui;
-use eframe::{egui::{CentralPanel, ScrollArea, Vec2}, App};
+use crate::headlines::{Headlines, PADDING};
+use eframe::egui::{self, Hyperlink, Label, RichText, TopBottomPanel, Ui};
+use eframe::{
+    egui::{CentralPanel, ScrollArea, Separator, Vec2},
+    App,
+};
 
 impl App for Headlines {
-   fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-       CentralPanel::default().show(ctx, |ui| {
-            ScrollArea::both().show(ui, |ui|{
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        self.render_top_panel(ctx);
+        CentralPanel::default().show(ctx, |ui| {
+            render_header(ui);
+            ScrollArea::both().show(ui, |ui| {
                 self.render_news_cards(ui);
             });
-       });
+            render_footer(ctx);
+        });
         self.configure_fonts(ctx);
-   }
+    }
+}
+
+fn render_footer(ctx: &egui::Context) {
+    TopBottomPanel::bottom("footer").show(ctx, |ui| {
+        ui.vertical_centered(|ui| {
+            ui.add_space(10.);
+
+            ui.add(Label::new(
+                RichText::new("API source: newsapi.org").monospace(),
+            ));
+
+            ui.add(Hyperlink::from_label_and_url(
+                RichText::new("Made with egui").text_style(eframe::egui::TextStyle::Monospace),
+                "https://github.com/emilk/egui",
+            ));
+
+            ui.add(Hyperlink::from_label_and_url(
+                RichText::new("vivek-verse/headlines-app")
+                    .text_style(eframe::egui::TextStyle::Monospace),
+                "https://github.com/vivek-verse/headlines-app",
+            ));
+
+            ui.add_space(10.);
+        });
+    });
+}
+
+fn render_header(ui: &mut Ui) {
+    ui.vertical_centered(|ui| {
+        ui.heading("headlines");
+    });
+    ui.add_space(PADDING);
+    let sep = Separator::default().spacing(20.);
+    ui.add(sep);
 }
 
 fn main() {
